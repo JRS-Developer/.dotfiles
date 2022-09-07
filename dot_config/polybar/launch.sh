327d23840c7dcda188fefdc4c0eb501a4c21897b
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-09-07 10:33:04 -0400

    lot of changes

---
+++ dot_config/polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+# polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+killall -q polybar
+
+# Launch bar1 and bar2
+echo "---"
+polybar left &
+polybar middle &
+polybar right &
+
+echo "Bars launched..."
